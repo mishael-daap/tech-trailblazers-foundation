d4661b2cd825d6d788a4ac3028db961bdffd3184
Mishael <themishaeldaap@gmail.com>
2026-05-08 11:37:56 +0100

    fiexed the images not showingon the gallery

--- src/utils/imageImports.ts
+++ src/utils/imageImports.ts
@@ -1,6 +1,6 @@
 const imageModules = import.meta.glob('../assets/wad-pictures/*.{webp,WEBP,png,PNG,jpg,JPG,jpeg,JPEG}', {
   eager: true,
-  as: 'url'
+  query: '?url',
+  import: 'default',
 });
-
 export const galleryImages = Object.values(imageModules) as string[];
